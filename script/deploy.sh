396582fde7256997b78d658d96b79f3badca0975
damaru-assets <damaru-assets@users.noreply.github.com>
2025-07-11 09:43:19 +0000

    add deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
+do_exit() {
+  cp build/apps-build.log /home/ubuntu/logs/ 
+  date
+  echo "$*"
+  rm -f ~/APPSBUILD.lock
+  exit 
+}
+
+
+cd /home/ubuntu/ASSETS/BUILD/public-apps
+
+sleep $(shuf -i 2-5 -n 1)
+
+
+lock(){
+  if [ -f ~/APPSBUILD.lock ]; then
+    echo "BUILD BUSY" "$(cat ~/APPSBUILD.lock)"
+    exit
+  fi
+  echo "PUBLIC APPS BUILD $(date)" | tee -a  ~/APPSBUILD.lock
+}
+
+
+(git fetch && git reset --hard HEAD && git merge origin/main) || (do_exit "git pull failed")
+
+scripts/should-build  || do_exit "no change"
+
+sh script/build-public-apps.sh prod
+rsync -avz build/* /home/ubuntu/ASSETS/www/apps/public/
+cp current-commit last-build-commit
